7c60e6cf3bff5aa3fa2141ab28cb0f2a27bcdbbf
PǝRV <alexander.w.matulionis@gmail.com>
2017-08-09 20:45:11 -0700

    Update golfer.go

--- golfer.go
+++ golfer.go
@@ -1,1 +1,14 @@
+package golfer
 
+import "github.com/gopherjs/gopherjs/js"
+
+// Lib appends a javascript library to the DOM and loads it.
+func Lib(src string) <-chan bool {
+	loaded := make(chan bool)
+	document := js.Global.Get("document")
+	script := document.Call("createElement", "script")
+	script.Set("src", src)
+	script.Set("onload", func() { loaded <- true })
+	document.Get("body").Call("appendChild", script)
+	return loaded
+}
