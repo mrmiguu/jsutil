8ec68a557b38aca304943092a06616bdacb4d401
PǝRV <alexander.w.matulionis@gmail.com>
2017-08-11 09:32:57 -0700

    Fixed text thing

--- golfer.go
+++ golfer.go
@@ -21,17 +21,20 @@ func Lib(src string) <-chan bool {
 }
 
 // Text creates a text element.
-func Text(name string) <-chan bool {
+func Text(id string) (*js.Object, <-chan bool) {
+	// <input type="text" id="FirstName" onclick="focus()" style="opacity: 0.0; position:absolute">
+
 	text := document.Call("createElement", "text")
-	text.Get("style").Set("visibility", "hidden")
-	text.Set("name", name)
+	text.Get("style").Set("opacity", 0.0)
+	text.Get("style").Set("position", "absolute")
+	text.Set("id", id)
 	clicked := make(chan bool, 1)
 	text.Set("onclick", func() {
 		text.Call("focus")
 		clicked <- true
 	})
 	body.Call("appendChild", text)
-	return clicked
+	return js.Global.Call("getElementById", id), clicked
 }
 
 // Callback returns a function that when run it fills the following channel.
