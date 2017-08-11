401dd3372b0254669efc71facc1a979cc1e7a24d
PǝRV <alexander.w.matulionis@gmail.com>
2017-08-11 09:55:59 -0700

    Added soft keyboard

--- golfer.go
+++ golfer.go
@@ -4,10 +4,20 @@ import "github.com/gopherjs/gopherjs/js"
 
 var document *js.Object
 var body *js.Object
+var keyboard *js.Object
 
 func init() {
 	document = js.Global.Get("document")
 	body = document.Get("body")
+
+	keyboard = document.Call("createElement", "text")
+	keyboard.Get("style").Set("opacity", 0.0)
+	keyboard.Get("style").Set("position", "absolute")
+	keyboard.Set("id", "keyboard")
+	keyboard.Set("onclick", func() {
+		keyboard.Call("focus")
+	})
+	body.Call("appendChild", keyboard)
 }
 
 // Lib appends a JavaScript library to the DOM and loads it.
@@ -20,22 +30,19 @@ func Lib(src string) <-chan bool {
 	return loaded
 }
 
-// Text creates a text element.
-func Text(id string) (*js.Object, <-chan bool) {
-	// <input type="text" id="FirstName" onclick="focus()" style="opacity: 0.0; position:absolute">
-
-	text := document.Call("createElement", "text")
-	text.Get("style").Set("opacity", 0.0)
-	text.Get("style").Set("position", "absolute")
-	text.Set("id", id)
-	clicked := make(chan bool, 1)
-	text.Set("onclick", func() {
-		text.Call("focus")
-		clicked <- true
-	})
-	body.Call("appendChild", text)
-	return js.Global.Call("getElementById", id), clicked
+// OpenKeyboard pulls up the soft keyboard.
+func OpenKeyboard() {
+	keyboard.Call("click")
 }
+
+// ReadKeyboard reads the text buffer from key input after an open.
+func ReadKeyboard() string {
+	text := keyboard.Get("value").String()
+	keyboard.Set("value", "")
+	return text
+}
+
+// <input type="text" id="FirstName" onclick="focus()" style="opacity: 0.0; position:absolute">
 
 // Callback returns a function that when run it fills the following channel.
 func Callback() (func(), <-chan bool) {
