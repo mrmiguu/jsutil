b70a140233e5b1bae0900b92a792887ddf6f6344
PǝRV <alexander.w.matulionis@gmail.com>
2017-08-12 17:13:25 -0700

    Golfer -> jsutil

--- jsutil.go
+++ jsutil.go
@@ -1,22 +1,25 @@
-package golfer
+package jsutil
 
 import "github.com/gopherjs/gopherjs/js"
 
-var document *js.Object
-var body *js.Object
-var keyboard *js.Object
+var (
+	document *js.Object
+	body     *js.Object
+	keyboard *js.Object
+)
 
 func init() {
 	document = js.Global.Get("document")
 	body = document.Get("body")
 
-	keyboard = document.Call("createElement", "text")
+	keyboard = document.Call("createElement", "input")
+	keyboard.Set("type", "text")
+	keyboard.Set("id", "keyboard")
+	keyboard.Get("style").Set("width", 0)
+	keyboard.Get("style").Set("left", -10)
 	keyboard.Get("style").Set("opacity", 0.0)
 	keyboard.Get("style").Set("position", "absolute")
-	keyboard.Set("id", "keyboard")
-	keyboard.Set("onclick", func() {
-		keyboard.Call("focus")
-	})
+	keyboard.Set("onclick", func() { keyboard.Call("focus") })
 	body.Call("appendChild", keyboard)
 }
 
@@ -31,18 +34,12 @@ func Lib(src string) <-chan bool {
 }
 
 // OpenKeyboard pulls up the soft keyboard.
-func OpenKeyboard() {
+func OpenKeyboard() <-chan string {
+	input := make(chan string, 1)
+	keyboard.Set("oninput", func() { input <- keyboard.Get("value").String() })
 	keyboard.Call("click")
+	return input
 }
-
-// ReadKeyboard reads the text buffer from key input after an open.
-func ReadKeyboard() string {
-	text := keyboard.Get("value").String()
-	keyboard.Set("value", "")
-	return text
-}
-
-// <input type="text" id="FirstName" onclick="focus()" style="opacity: 0.0; position:absolute">
 
 // Callback returns a function that when run it fills the following channel.
 func Callback() (func(), <-chan bool) {
